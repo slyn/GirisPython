8f80b645dbb7a1d8f7fe09f8a5e29c564221bac0
Hüsnü İŞLEYEN <husnuisleyen@gmail.com>
2014-04-29 00:13:31 +0300

    sayiMi kontrolu

---
+++ SayiMi.py
@@ -0,0 +1,21 @@
+#SAYI MI
+# ',' ve '.' float sayilar icin
+numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, '.', ','}
+
+
+def is_digit(text):
+    for i in text:
+        if i == ' ':
+            return False
+        if i in str(numbers):
+            pass
+        else:
+            return False
+    return True
+
+
+
+if is_digit('156,4'):
+    print('SAYI')
+else:
+    print('TEXT')

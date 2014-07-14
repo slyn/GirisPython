3250c473050c14ccfbd12011f5fe443c1dea4438
enis <enisozgen@hotmail.com>
2014-07-14 19:14:56 +0300

    pep8 standartlarinda yazildi

--- NETWORK/ClientSide.py
+++ NETWORK/ClientSide.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
-#CLIENT SIDE
+# CLIENT SIDE
 import socket
 
-#Soket nesnesi
+# Soket nesnesi
 s = socket.socket()
 host = socket.gethostname()
 port = 55555
 
-s.connect((host,port))
+s.connect((host, port))
 print(s.recv(1024))
 s.close()
